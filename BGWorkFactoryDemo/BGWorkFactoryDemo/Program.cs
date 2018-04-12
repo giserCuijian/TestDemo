9aa5775aa565c8ea249ded0e620cae53a6bb4a36
giserCuijian <38305218+giserCuijian@users.noreply.github.com>
2018-04-12 15:47:32 +0800

    变更工厂
    变更工厂

---
+++ BGWorkFactoryDemo/BGWorkFactoryDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGWorkFactoryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BGAlter bgAlter = null;
+
+            ////使用权宗地变更
+            BGFactory syqFac = new SYQFactory();
+            bgAlter = syqFac.GetAlterObj();
+
+            ////所有权宗地变更
+            BGFactory suqFac = new SUQFactory();
+            bgAlter = suqFac.GetAlterObj();
+
+
+        }
+    }
+}
---
+++ BGWorkFactoryDemo/BGWorkFactoryDemo/unt_BGAlter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BGWorkFactoryDemo
+{
+    /// <summary>
+    /// 变更对象
+    /// </summary>
+    public abstract class BGAlter
+    {
+        public abstract void DoNum();
+    }
+
+    /// <summary>
+    /// 使用权宗地变更
+    /// </summary>
+    public class SYQBGAlter : BGAlter
+    {
+        public override void DoNum()
+        {
+            Console.WriteLine("使用权宗地编号。");
+        }
+    }
+
+    /// <summary>
+    /// 所有权宗地变更
+    /// </summary>
+    public class SUQBGAlter : BGAlter
+    {
+        public override void DoNum()
+        {
+            Console.WriteLine("所有权宗地编号。");
+        }
+    }
+
+    /// <summary>
+    /// 变更工厂
+    /// </summary>
+    public abstract class BGFactory
+    {
+        public abstract BGAlter GetAlterObj();
+    }
+
+    /// <summary>
+    /// 使用权变更车间
+    /// </summary>
+    public class SYQFactory : BGFactory
+    {
+        public override BGAlter GetAlterObj()
+        {
+            return new SYQBGAlter();
+        }
+    }
+
+    /// <summary>
+    /// 所有权变更车间
+    /// </summary>
+    public class SUQFactory : BGFactory
+    {
+        public override BGAlter GetAlterObj()
+        {
+            return new SUQBGAlter();
+        }
+    }
+
+}
